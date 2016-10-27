38e388f8386ebfaf3d15b36d226c3e9eb2cde229
gustavekd <gustav.ekelund99@gmail.com>
2016-10-27 17:52:42 +0200

    ds

--- Nästlade loopar/Program.cs
+++ Nästlade loopar/Program.cs
@@ -10,6 +10,72 @@ namespace Nästlade_loopar
     {
         static void Main(string[] args)
         {
+            //övning1
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    Console.Write("o#");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" ");
+            //övning2
+
+            Console.WriteLine(" ");
+
+            for (int a = 0; a < 11; a++)
+            {
+                for (int b = 0; b < 11; b++)
+                {
+                    if ((b + a) % 2 == 0)
+                    {
+                        Console.Write("o");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+
+            //övning3
+            Console.WriteLine(" ");
+
+            for (int a = 0; a < 11; a++)
+            {
+                for (int b = a; b < 11; b++)
+
+
+                {
+                    Console.Write("#");
+                }
+                   Console.WriteLine();
+            }
+
+            //övning4
+            Console.WriteLine(" ");
+
+            for (int a = 0; a < 7; a++)
+            {
+                for (int b = 0; b < 7; b++)
+                {
+                    if ( a - b < 0)
+                        Console.Write("*");
+                    else
+                        Console.Write(".");              
+
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
+    
+    
+
